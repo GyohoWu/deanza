3d51c3282124af21124f41dab6c8c443f824ed50
Yaofeng Wu <yaofeng@frontier.local>
2019-01-05 17:37:09 -0800

    done

--- app/models/classpost.rb
+++ app/models/classpost.rb
@@ -1,6 +1,5 @@
 class Classpost < ApplicationRecord
     validates :prof, {presence:true, length:{maximum: 20}}
-    validates :source, {presence:true, length:{maximum: 120}}
     validates :comment, {presence:true, length:{maximum: 350, minimum: 20}}
     validates :success, {presence:true, length:{maximum: 350, minimum: 20}}
     validates :rate, {presence:true}
